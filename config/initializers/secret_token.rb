b7faaab63070bbcea8e0fa9dd13853c6c5c693f3
Mohammad R. Alansari <mohammad.alansari@dictionaryproject.org>
2011-05-16 15:19:50 -0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4e1ccfbfaafa178658292cf67bfe755adc2cfff98468680c58960d38999ea3f331ce53d87520b6c2eb7896486159694e1c47353e3a3620292e48d01882f0efec'
